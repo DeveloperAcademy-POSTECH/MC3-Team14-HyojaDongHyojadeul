1dd060ab63dd48bacb62df8d5e51c74a85539099
Goban <gobanest@gmail.com>
2022-07-17 13:51:38 +0900

    [Chore] ViewController와 ColorExtension추가

---
+++ AMaDda/Screens/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  AMaDda
+//
+//  Created by Lee Myeonghwan on 2022/07/17.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.amaddaBackgroundColor
+    }
+}
+
---
+++ AMaDda/Screens/Main/Views/TodayQuestion.swift
@@ -0,0 +1,15 @@
+//
+//  TodayQuestion.swift
+//  AMaDda
+//
+//  Created by Lee Myeonghwan on 2022/07/17.
+//
+
+import UIKit
+
+final class TodayQuestion {
+
+    func configureAddSubViewsTodayQuestionView() {
+        
+    }
+}
