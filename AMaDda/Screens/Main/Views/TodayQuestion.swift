6ceb78d544b6aea98fa8038a0918ef2ac2731ede
Goban <gobanest@gmail.com>
2022-07-17 20:10:35 +0900

    [CHORE] seperate mock data

--- AMaDda/Screens/Main/Views/TodayQuestion.swift
+++ AMaDda/Screens/Main/Views/TodayQuestion.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 final class TodayQuestion {
-    private let todayQuestionData: [TodayQuestionData] = [
-        TodayQuestionData(question: "Q. 오늘 가족들이 먹은 점심은 무엇인가요?"),
-        TodayQuestionData(question: "Q. 가족들의 MBTI는?"),
-        TodayQuestionData(question: "Q. 고반의 생일은?"),
-        TodayQuestionData(question: "Q. 이번 주말 가족들의 계획은?")
-    ]
+    private let todayQuestionData = TodayQuestionMockData.mockData
     // MARK: - property
     let todayQuestionView = UIView()
     private let todayTitleLabel: UILabel = {
