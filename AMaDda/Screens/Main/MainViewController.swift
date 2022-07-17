6639a02967026144a6a2ba9c3e31a1a954fe727f
Goban <gobanest@gmail.com>
2022-07-17 14:21:41 +0900

    [FEAT] todayQuestionUI 작업완료

--- AMaDda/Screens/Main/MainViewController.swift
+++ AMaDda/Screens/Main/MainViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-    
+    private let todayQuestion = TodayQuestion()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.amaddaBackgroundColor
+        configureUI()
+        configureAddSubViews()
+        configureConstarints()
     }
 }
 
+extension MainViewController {
+    private func configureUI() {
+        guard let view = self.view else { return }
+        view.backgroundColor = UIColor.amaddaBackgroundColor
+    }
+    private func configureAddSubViews() {
+        guard let view = self.view else { return }
+        view.addSubview(todayQuestion.todayQuestionView)
+    }
+    private func configureConstarints() {
+        todayQuestion.todayQuestionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            todayQuestion.todayQuestionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            todayQuestion.todayQuestionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            todayQuestion.todayQuestionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            todayQuestion.todayQuestionView.heightAnchor.constraint(equalToConstant: 160),
+        ])
+        todayQuestion.configureAddSubViewsTodayQuestionView()
+        todayQuestion.configureConstraintsTodayQuestionView()
+    }
+}
--- AMaDda/Screens/Main/Views/TodayQuestion.swift
+++ AMaDda/Screens/Main/Views/TodayQuestion.swift
@@ -8,8 +8,49 @@
 import UIKit
 
 final class TodayQuestion {
-
+    let todayQuestionView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    let todayTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "오늘의 가족 Question"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    let todayCardView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.backgroundColor = UIColor.cardBackgroundColor
+        return view
+    }()
+    let todayCardQuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Q. 오늘 가족들이 먹은 점심은 무엇인가요?"
+        return label
+    }()
     func configureAddSubViewsTodayQuestionView() {
-        
+        todayQuestionView.addSubview(todayTitleLabel)
+        todayQuestionView.addSubview(todayCardView)
+        todayCardView.addSubview(todayCardQuestionLabel)
+    }
+    func configureConstraintsTodayQuestionView() {
+        todayTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            todayTitleLabel.topAnchor.constraint(equalTo: todayQuestionView.safeAreaLayoutGuide.topAnchor),
+            todayTitleLabel.leadingAnchor.constraint(equalTo: todayQuestionView.safeAreaLayoutGuide.leadingAnchor)
+        ])
+        todayCardView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            todayCardView.topAnchor.constraint(equalTo: todayTitleLabel.safeAreaLayoutGuide.bottomAnchor, constant: 28),
+            todayCardView.leadingAnchor.constraint(equalTo: todayQuestionView.safeAreaLayoutGuide.leadingAnchor),
+            todayCardView.trailingAnchor.constraint(equalTo: todayQuestionView.safeAreaLayoutGuide.trailingAnchor),
+            todayCardView.bottomAnchor.constraint(equalTo: todayQuestionView.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        todayCardQuestionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            todayCardQuestionLabel.centerYAnchor.constraint(equalTo: todayCardView.centerYAnchor),
+            todayCardQuestionLabel.centerXAnchor.constraint(equalTo: todayCardView.centerXAnchor)
+        ])
     }
 }
