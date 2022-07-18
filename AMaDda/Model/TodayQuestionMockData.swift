d1c68d81365eea30916fe6a72695a30e480ff3c2
Goban <gobanest@gmail.com>
2022-07-18 11:03:29 +0900

    [FEAT][CHORE]가족 퀘스천 UI구현 및 group 플레이스홀더 생성

---
+++ AMaDda/Model/TodayQuestionMockData.swift
@@ -0,0 +1,22 @@
+//
+//  TodayQuestionMockData.swift
+//  AMaDda
+//
+//  Created by Lee Myeonghwan on 2022/07/18.
+//
+
+import Foundation
+
+#if DEBUG
+final class TodayQuestionMockData {
+    static var mockData: [TodayQuestionData] {
+        let data = [
+            TodayQuestionData(question: "Q. 오늘 가족들이 먹은 점심은 무엇인가요?"),
+            TodayQuestionData(question: "Q. 가족들의 MBTI는?"),
+            TodayQuestionData(question: "Q. 고반의 생일은?"),
+            TodayQuestionData(question: "Q. 이번 주말 가족들의 계획은?")
+        ]
+        return data
+    }
+}
+#endif
