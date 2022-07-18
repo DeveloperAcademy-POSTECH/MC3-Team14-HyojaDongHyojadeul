60d0024b277f3ba903c24897eb351552743a81f3
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-07-18 17:53:02 +0900

    [FEAT] AddingViewController UI구현(#33)

---
+++ AMaDda/Global/Literal/ImageLiteral.swift
@@ -0,0 +1,42 @@
+//
+//  ImageLiteral.swift
+//  AMaDda
+//
+//  Created by 이성호 on 2022/07/18.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    
+    // MARK: - icon
+    static var icPlus: UIImage { .load(systemName: "plus") }
+    
+    // MARK: - button
+    static var btnProfile: UIImage { .load(systemName: "person.circle.fill") }
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+    static func load(systemName: String) -> UIImage {
+        guard let image = UIImage(systemName: systemName, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = systemName
+        return image
+    }
+    
+    func resize(to size: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}
--- AMaDda/Screens/Adding/AddingViewController.swift
+++ AMaDda/Screens/Adding/AddingViewController.swift
@@ -10,10 +10,59 @@ import UIKit
 class AddingViewController: UIViewController {
     
     // MARK: - property
-    
+    private let addingTitle: UILabel = {
+        let label = UILabel()
+        label.text = "추가하기"
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        return label
+    }()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.btnProfile
+        imageView.tintColor = .lightGray
+        return imageView
+    }()
+    private let plusIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = ImageLiterals.icPlus
+        imageView.tintColor = .gray
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    private let createNicknameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "어떻게 부르고 싶나요?"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "예시) 아버지, 엄마, 세젤예 우리엄마"
+        return textField
+    }()
+    private let underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    private let textFieldLimitLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0/10"
+        label.textColor = .lightGray
+        return label
+    }()
+    private let addButton: CommonButton = {
+        let button = CommonButton()
+        button.title = "추가하기"
+        return button
+    }()
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configureAddSubView()
+        configureConstraints()
     }
 
     // MARK: - configure
@@ -22,10 +71,65 @@ class AddingViewController: UIViewController {
     }
     
     private func configureAddSubView() {
-        
+        view.addSubview(addingTitle)
+        view.addSubview(imageView)
+        view.addSubview(plusIcon)
+        view.addSubview(createNicknameLabel)
+        view.addSubview(textField)
+        view.addSubview(underLineView)
+        view.addSubview(textFieldLimitLabel)
+        view.addSubview(addButton)
     }
     
     private func configureConstraints() {
-        
+        addingTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addingTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            addingTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: Size.leadingTrailingPadding),
+        ])
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: addingTitle.bottomAnchor, constant: 37),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+        ])
+        plusIcon.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            plusIcon.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0),
+            plusIcon.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 0),
+            plusIcon.widthAnchor.constraint(equalToConstant: 25),
+            plusIcon.heightAnchor.constraint(equalToConstant: 25),
+        ])
+        createNicknameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            createNicknameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor,constant: 64),
+            createNicknameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Size.leadingTrailingPadding),
+        ])
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: createNicknameLabel.bottomAnchor, constant: 20),
+            textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: Size.leadingTrailingPadding),
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Size.leadingTrailingPadding),
+            textField.heightAnchor.constraint(equalToConstant: 60),
+        ])
+        underLineView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            underLineView.topAnchor.constraint(equalTo: textField.bottomAnchor),
+            underLineView.leadingAnchor.constraint(equalTo: textField.leadingAnchor),
+            underLineView.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+            underLineView.heightAnchor.constraint(equalToConstant: 1),
+        ])
+        textFieldLimitLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textFieldLimitLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 5),
+            textFieldLimitLabel.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+        ])
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -42),
+            addButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Size.leadingTrailingPadding),
+            addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Size.leadingTrailingPadding),
+        ])
     }
 }
