0670c0a2ceda9f03f7cd2c184235cce952d18811
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-07-18 17:03:41 +0900

    [INIT] AddingView 초기 생성(#33)

---
+++ AMaDda/Screens/Adding/AddingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddingViewController.swift
+//  AMaDda
+//
+//  Created by 이성호 on 2022/07/18.
+//
+
+import UIKit
+
+class AddingViewController: UIViewController {
+    
+    // MARK: - property
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - configure
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func configureAddSubView() {
+        
+    }
+    
+    private func configureConstraints() {
+        
+    }
+}
