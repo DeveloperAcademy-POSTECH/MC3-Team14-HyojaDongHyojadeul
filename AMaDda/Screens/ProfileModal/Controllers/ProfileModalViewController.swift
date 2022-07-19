56a5c40adbd91993c4ee4fd588b5d054d629bc7a
SH0123 <sanghyoboy@naver.com>
2022-07-19 23:22:47 +0900

    [FEAT]collectionView cell 및 select 기능 구현

--- AMaDda/Screens/ProfileModal/Controllers/ProfileModalViewController.swift
+++ AMaDda/Screens/ProfileModal/Controllers/ProfileModalViewController.swift
@@ -8,22 +8,49 @@
 import UIKit
 import SwiftUI
 
+// TODO: edit, add viewController에서 delegate 구현 필요
+protocol profileModalViewDelegate: AnyObject {
+    func registerSelectedCharacter(imageName: String)
+}
+
 final class ProfileModalViewController: UIViewController {
-    
     // MARK: - Properties
-    enum Size {
+    private enum Size {
         static let horizontalInset: CGFloat = 20.0
-        static let collectionInterItemSize: CGFloat = 5.0
+        static let collectionInterItemSize: CGFloat = 20.0
     }
+    private enum BarButtonStyle: Int {
+        case cancel, register
+    }
+    // TODO: 이미지 이름 바인딩 필요
+    var selectedCharacterName = "Character1"
+    private var postfixNums = Array<Int>(1...8)
+    private lazy var characterNames: [String] = {
+        let names = postfixNums.map {
+            "Character" + String($0)
+        }
+        return names
+    }()
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(ProfileModalCell.self, forCellWithReuseIdentifier: ProfileModalCell.identifier)
+        collectionView.register(ProfileModalCell.self, forCellWithReuseIdentifier: ProfileModalCell.className)
         collectionView.contentInset = UIEdgeInsets(top: 0, left: Size.horizontalInset, bottom: 0, right: Size.horizontalInset)
         collectionView.dataSource = self
         collectionView.delegate = self
         return collectionView
     }()
+    private lazy var cancelBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(tapBarButton(_:)))
+        button.tag = 0
+        return button
+    }()
+    private lazy var registerBarButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "등록", style: .plain, target: self, action: #selector(tapBarButton(_:)))
+        button.tag = 1
+        return button
+    }()
+    weak var delegate: profileModalViewDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +62,10 @@ final class ProfileModalViewController: UIViewController {
     // MARK: - configure
     private func configureUI() {
         view.backgroundColor = .systemBackground
+        self.navigationItem.leftBarButtonItem = cancelBarButton
+        self.navigationItem.rightBarButtonItem = registerBarButton
     }
+    
     private func configureAddSubviews() {
         view.addSubview(collectionView)
     }
@@ -50,17 +80,40 @@ final class ProfileModalViewController: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    // MARK: - Function
+    private func getImageIndex(by name: String) -> Int {
+        guard let index = characterNames.firstIndex(of: name) else { fatalError() }
+        return index
+    }
+    
+    // MARK: - Selector
+    @objc func tapBarButton(_ sender: Any) {
+        guard let sender = sender as? UIBarButtonItem else { return }
+        if sender.tag == 1 {
+            // TODO: 데이터 바인딩 방법에 따른 코드 구현
+            let imageIndex = getImageIndex(by: selectedCharacterName)
+            self.delegate?.registerSelectedCharacter(imageName: characterNames[imageIndex])
+        }
+        navigationController?.popViewController(animated: true)
+    }
 }
 
 // MARK: - UICollectionViewDatasource
 extension ProfileModalViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //TODO: 이미지 이름 배열에 따른 개수 출력
-        return 9
+        return characterNames.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileModalCell.identifier, for: indexPath) as? ProfileModalCell else { fatalError() }
-        cell.backgroundColor = .green
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileModalCell.className, for: indexPath) as? ProfileModalCell else { fatalError() }
+        let imageIndex = getImageIndex(by: selectedCharacterName)
+        if indexPath.row == imageIndex {
+            cell.isSelected = true
+            collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .init())
+        }
+        cell.isSelected = indexPath.row == imageIndex
+        cell.imageName = characterNames[indexPath.row]
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
@@ -78,9 +131,10 @@ extension ProfileModalViewController: UICollectionViewDelegateFlowLayout {
 }
 
 // MARK: - Preview
+#if DEBUG
 struct ProfileView: PreviewProvider {
     static var previews: some View {
         ProfileModalViewController().toPreview()
     }
 }
-
+#endif
--- AMaDda/Screens/ProfileModal/Views/ProfileModalCell.swift
+++ AMaDda/Screens/ProfileModal/Views/ProfileModalCell.swift
@@ -7,8 +7,64 @@
 
 import UIKit
 
-class ProfileModalCell: UICollectionViewCell {
-    static let identifier = "profileCellIdentifier"
+final class ProfileModalCell: UICollectionViewCell {
+    // MARK: - Property
     
-    //TODO: 아마따 고양이 이미지 + 체크 이미지를 포함할 stackView 생성
+    override var isSelected: Bool {
+        didSet {
+            checkMarkImageView.isHidden = !isSelected
+        }
+    }
+    var imageName: String = "Character1" {
+        didSet {
+            profileImageView.image = .load(name: imageName)
+        }
+    }
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let checkMarkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "checkmark.circle.fill")
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .black
+        imageView.isHidden = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureAddSubviews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    private func configureAddSubviews() {
+        contentView.addSubviews(profileImageView,
+                                checkMarkImageView)
+    }
+    
+    private func configureConstraints() {
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            profileImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            profileImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            profileImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+        
+        checkMarkImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            checkMarkImageView.topAnchor.constraint(equalTo: profileImageView.topAnchor, constant: 5),
+            checkMarkImageView.trailingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: -5),
+            checkMarkImageView.widthAnchor.constraint(equalToConstant: 40),
+            checkMarkImageView.heightAnchor.constraint(equalTo: checkMarkImageView.widthAnchor),
+        ])
+    }
 }
