5eac6e8065af2511236170c2acb852c4dfd40ac3
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2022-07-19 17:14:37 +0900

    [CHORE] NS레이아웃쪽 코드 한줄 띄우기

--- AMaDda/Screens/Adding/AddingViewController.swift
+++ AMaDda/Screens/Adding/AddingViewController.swift
@@ -146,6 +146,7 @@ class AddingViewController: UIViewController {
             addingTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             addingTitleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: Size.leadingTrailingPadding),
         ])
+        
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -153,6 +154,7 @@ class AddingViewController: UIViewController {
             profileImageView.widthAnchor.constraint(equalToConstant: 100),
             profileImageView.heightAnchor.constraint(equalToConstant: 100),
         ])
+        
         plusIcon.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             plusIcon.bottomAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 0),
@@ -160,11 +162,13 @@ class AddingViewController: UIViewController {
             plusIcon.widthAnchor.constraint(equalToConstant: 25),
             plusIcon.heightAnchor.constraint(equalToConstant: 25),
         ])
+        
         createNicknameLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             createNicknameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor,constant: 64),
             createNicknameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Size.leadingTrailingPadding),
         ])
+        
         nickNameTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             nickNameTextField.topAnchor.constraint(equalTo: createNicknameLabel.bottomAnchor, constant: 20),
@@ -172,6 +176,7 @@ class AddingViewController: UIViewController {
             nickNameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Size.leadingTrailingPadding),
             nickNameTextField.heightAnchor.constraint(equalToConstant: 50),
         ])
+        
         underLineView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             underLineView.topAnchor.constraint(equalTo: nickNameTextField.bottomAnchor),
@@ -179,17 +184,20 @@ class AddingViewController: UIViewController {
             underLineView.trailingAnchor.constraint(equalTo: nickNameTextField.trailingAnchor),
             underLineView.heightAnchor.constraint(equalToConstant: 1),
         ])
+        
         textFieldLimitLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             textFieldLimitLabel.topAnchor.constraint(equalTo: nickNameTextField.bottomAnchor, constant: 5),
             textFieldLimitLabel.trailingAnchor.constraint(equalTo: nickNameTextField.trailingAnchor),
         ])
+        
         addButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -42),
             addButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Size.leadingTrailingPadding),
             addButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Size.leadingTrailingPadding),
         ])
+        
     }
 }
 
