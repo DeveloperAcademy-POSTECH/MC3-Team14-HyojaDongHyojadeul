8f625dd9d1934a25f3669623f5fdc3ca5ba2e9fc
seongmin221 <seongmin221@naver.com>
2022-07-20 22:52:41 +0900

    [CHORE] other viewcontrollers added

--- AMaDda/Global/Supports/SceneDelegate.swift
+++ AMaDda/Global/Supports/SceneDelegate.swift
@@ -17,10 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-         window = UIWindow(windowScene: windowScene)
-         let mainVC = EdittingViewController()
-         window?.rootViewController = mainVC
-         window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+//        let mainVC = OnboardingOneViewController()
+//        let mainVC = OnboardingTwoViewController()
+//        let mainVC = MainViewController()
+//        let mainVC = AddingViewController()
+        let mainVC = EdittingViewController()
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
