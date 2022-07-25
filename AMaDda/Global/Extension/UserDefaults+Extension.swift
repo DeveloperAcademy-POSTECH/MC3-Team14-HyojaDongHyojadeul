74c32d2e46ac76360fb7c2fd687f83389a69bd53
Goban <gobanest@gmail.com>
2022-07-25 16:49:47 +0900

    [CHORE]TextLiteral 추가

--- AMaDda/Global/Extension/UserDefaults+Extension.swift
+++ AMaDda/Global/Extension/UserDefaults+Extension.swift
@@ -10,40 +10,40 @@ import Foundation
 extension UserDefaults {
     var userNotificationCycle: Int? {
         get {
-            guard let count = UserDefaults.standard.value(forKey: "userNotificationCycle") as? Int else { return nil }
+            guard let count = UserDefaults.standard.value(forKey: TextLiteral.userNotificationCycle) as? Int else { return nil }
             return count
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "userNotificationCycle")
+            UserDefaults.standard.set(newValue, forKey: TextLiteral.userNotificationCycle)
         }
     }
     var finalContactDiffDay: Int? {
         get {
-            guard let diff = UserDefaults.standard.value(forKey: "finalContactDiffDay") as? Int else { return nil }
+            guard let diff = UserDefaults.standard.value(forKey: TextLiteral.finalContactDiffDay) as? Int else { return nil }
             return diff
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "finalContactDiffDay")
+            UserDefaults.standard.set(newValue, forKey: TextLiteral.finalContactDiffDay)
         }
     }
     var finalEnteredDate: Date? {
         get {
-            guard let enteredDate = UserDefaults.standard.value(forKey: "finalEnteredDate") as? Date else {
+            guard let enteredDate = UserDefaults.standard.value(forKey: TextLiteral.finalEnteredDate) as? Date else {
                 return nil
             }
             return enteredDate
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "finalEnteredDate")
+            UserDefaults.standard.set(newValue, forKey: TextLiteral.finalEnteredDate)
         }
     }
     var questionIndex: Int {
         get {
-            guard let index = UserDefaults.standard.value(forKey: "questionIndex") as? Int else { return 0 }
+            guard let index = UserDefaults.standard.value(forKey: TextLiteral.questionIndex) as? Int else { return 0 }
             return index
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "questionIndex")
+            UserDefaults.standard.set(newValue, forKey: TextLiteral.questionIndex)
         }
     }
 }
