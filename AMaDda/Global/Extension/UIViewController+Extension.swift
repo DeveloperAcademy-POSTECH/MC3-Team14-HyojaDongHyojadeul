1a10ee5822fa8ac70872b05fc26d008af4b9f765
SH0123 <sanghyoboy@naver.com>
2022-07-28 23:15:59 +0900

    [BUG] 버튼 무시되는 문제 해결

--- AMaDda/Global/Extension/UIViewController+Extension.swift
+++ AMaDda/Global/Extension/UIViewController+Extension.swift
@@ -27,14 +27,20 @@ extension UIViewController {
 }
 #endif
 
-extension UIViewController {
+extension UIViewController: UIGestureRecognizerDelegate {
     func hidekeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
+        tap.delegate = self
         view.addGestureRecognizer(tap)
     }
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        guard touch.view is UIButton else { return true }
+        return false
     }
 }
